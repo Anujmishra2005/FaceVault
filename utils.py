82a0a4632a70f742dab8608e0ca27363341ff717
Anujmishra2005 <anujmishraan2005@gmail.com>
2025-08-03 23:48:31 +0530

    New Commit

---
+++ utils.py
@@ -0,0 +1,7 @@
+def menu_ui():
+    print("\n===== FaceVault Menu =====")
+    print("1. Register New Face")
+    print("2. Login with Face")
+    print("3. Detect Emotion")
+    print("4. Exit")
+    return input("Enter your choice: ")
