0cab6c559ba12e5a1d35e7229758afb120d5a0b9
Anujmishra2005 <anujmishraan2005@gmail.com>
2025-08-03 23:46:37 +0530

    New Commit

---
+++ facevault_main.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import face_register
+import face_login
+import emotion_detector
+from logger import Logger
+from utils import menu_ui
+
+if __name__ == "__main__":
+    logger = Logger()
+    while True:
+        choice = menu_ui()
+
+        if choice == '1':
+            face_register.register_user(logger)
+
+        elif choice == '2':
+            face_login.login_user(logger)
+
+        elif choice == '3':
+            emotion_detector.detect_emotion()
+
+        elif choice == '4':
+            print("[INFO] Exiting FaceVault. Goodbye!")
+            sys.exit()
+
+        else:
+            print("[ERROR] Invalid choice. Please try again.")
